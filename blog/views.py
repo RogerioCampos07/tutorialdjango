9a14febac8b4fc365b819a45cd32a6db0aa8b8fb
Rogerio Campos <rogerio.campos07@gmail.com>
2022-04-23 18:44:29 -0300

    Adicionado as views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import DetailView,ListView
+from .models import Post
+
+class PostView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
 
 # Create your views here.
